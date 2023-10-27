84d2df56ac3cee532dceaaeff536325bc4b2c24b
RickDeb2004 <debanjanrick04@gmail.com>
2023-10-27 13:16:16 +0530

    chore: Update
    fix: -- a/main.go
    feat: ++ b/main.go
    feat: "strings"
    feat: func generateDynamicCommitMessage() string {
    feat: // Use "git diff --cached" to get the staged changes
    feat: cmd := exec.Command("git", "diff", "--cached")
    feat: output, err := cmd.Output()
    feat: if err != nil {
    feat: log.Fatalf("Failed to execute 'git diff --cached': %v", err)
    feat: }
    feat:
    feat: // Process the output to create a commit message
    feat: lines := strings.Split(string(output), "\n")
    feat: commitMessage := "chore: Update\n\n" // Default message if no changes
    feat:
    feat: for _, line := range lines {
    feat: if strings.HasPrefix(line, "+") {
    feat: // This line represents an addition, so include it in the commit message
    feat: commitMessage += "feat: " + strings.TrimSpace(line[1:]) + "\n"
    feat: } else if strings.HasPrefix(line, "-") {
    feat: // This line represents a deletion, so include it in the commit message
    feat: commitMessage += "fix: " + strings.TrimSpace(line[1:]) + "\n"
    feat: }
    feat: }
    feat:
    feat: // Remove the default line if changes were detected
    feat: if commitMessage != "chore: Update\n\n" {
    feat: return commitMessage
    feat: }
    feat:
    feat: // No changes detected
    feat: return "chore: No significant changes\n\n"
    feat: }
    feat:
    feat: // func updateCommitPush() {
    feat: // 	if err := executeCommand("git", "add", "-A"); err != nil {
    feat: // 		log.Fatalf("Failed to execute 'git add' command: %v", err)
    feat: // 	}
    feat:
    feat: // 	name := nameGenerator()
    feat: // 	if err := executeCommand("git", "commit", "-m", name); err != nil {
    feat: // 		log.Fatalf("Failed to execute 'git commit' command: %v", err)
    feat: // 	}
    feat:
    feat: // 	if err := executeCommand("git", "push", "origin", "master"); err != nil {
    feat: // 		log.Fatalf("Failed to execute 'git push' command: %v", err)
    feat: // 	}
    feat:
    feat: // 	fmt.Println("Successfully added, committed, and pushed all the changes")
    feat: // }
    feat:
    feat: // func nameGenerator() string {
    feat:
    feat: // 	return "initialCommit"
    feat: // }
    feat:
    feat: //	func main() {
    feat: //		updateCommitPush()
    feat: //	}
    feat: // Get the dynamic commit message
    feat: commitMessage := generateDynamicCommitMessage()
    feat:
    feat: // Perform the Git add, commit, and push with the dynamic commit message
    fix: name := nameGenerator()
    fix: if err := executeCommand("git", "commit", "-m", name); err != nil {
    feat: if err := executeCommand("git", "commit", "-m", commitMessage); err != nil {
    fix: fmt.Println("Successfully added, committed, and pushed all the changes")
    feat: fmt.Println("Successfully added, committed, and pushed all the changes with dynamic commit message")
    fix:
    fix: func nameGenerator() string {
    fix:
    fix: return "initialCommit"
    fix: }
    fix:

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 func executeCommand(command string, args ...string) error {
@@ -14,13 +15,72 @@ func executeCommand(command string, args ...string) error {
 	return cmd.Run()
 }
 
+func generateDynamicCommitMessage() string {
+	// Use "git diff --cached" to get the staged changes
+	cmd := exec.Command("git", "diff", "--cached")
+	output, err := cmd.Output()
+	if err != nil {
+		log.Fatalf("Failed to execute 'git diff --cached': %v", err)
+	}
+
+	// Process the output to create a commit message
+	lines := strings.Split(string(output), "\n")
+	commitMessage := "chore: Update\n\n" // Default message if no changes
+
+	for _, line := range lines {
+		if strings.HasPrefix(line, "+") {
+			// This line represents an addition, so include it in the commit message
+			commitMessage += "feat: " + strings.TrimSpace(line[1:]) + "\n"
+		} else if strings.HasPrefix(line, "-") {
+			// This line represents a deletion, so include it in the commit message
+			commitMessage += "fix: " + strings.TrimSpace(line[1:]) + "\n"
+		}
+	}
+
+	// Remove the default line if changes were detected
+	if commitMessage != "chore: Update\n\n" {
+		return commitMessage
+	}
+
+	// No changes detected
+	return "chore: No significant changes\n\n"
+}
+
+// func updateCommitPush() {
+// 	if err := executeCommand("git", "add", "-A"); err != nil {
+// 		log.Fatalf("Failed to execute 'git add' command: %v", err)
+// 	}
+
+// 	name := nameGenerator()
+// 	if err := executeCommand("git", "commit", "-m", name); err != nil {
+// 		log.Fatalf("Failed to execute 'git commit' command: %v", err)
+// 	}
+
+// 	if err := executeCommand("git", "push", "origin", "master"); err != nil {
+// 		log.Fatalf("Failed to execute 'git push' command: %v", err)
+// 	}
+
+// 	fmt.Println("Successfully added, committed, and pushed all the changes")
+// }
+
+// func nameGenerator() string {
+
+// 	return "initialCommit"
+// }
+
+//	func main() {
+//		updateCommitPush()
+//	}
 func updateCommitPush() {
+	// Get the dynamic commit message
+	commitMessage := generateDynamicCommitMessage()
+
+	// Perform the Git add, commit, and push with the dynamic commit message
 	if err := executeCommand("git", "add", "-A"); err != nil {
 		log.Fatalf("Failed to execute 'git add' command: %v", err)
 	}
 
-	name := nameGenerator()
-	if err := executeCommand("git", "commit", "-m", name); err != nil {
+	if err := executeCommand("git", "commit", "-m", commitMessage); err != nil {
 		log.Fatalf("Failed to execute 'git commit' command: %v", err)
 	}
 
@@ -28,14 +88,8 @@ func updateCommitPush() {
 		log.Fatalf("Failed to execute 'git push' command: %v", err)
 	}
 
-	fmt.Println("Successfully added, committed, and pushed all the changes")
+	fmt.Println("Successfully added, committed, and pushed all the changes with dynamic commit message")
 }
-
-func nameGenerator() string {
-
-	return "initialCommit"
-}
-
 func main() {
 	updateCommitPush()
 }
